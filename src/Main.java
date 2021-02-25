215c69358accb01e4f60b609939f1c47481cc226
macmar22 <maciej.marucha@gmail.com>
2021-02-26 00:40:31 +0100

    A

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        Interfejs interfejs = new Klasaimplementujaca();
-        System.out.println(Klasaimplementujaca.class);
-        Object obj = interfejs;
-        Class<?> cl = obj.getClass();
-        //zmiana na drugim konc
-        //zmiana repo 1
-        //zmianadruga repo 1
+        //znieszczenie wszystkiego
     }
 }
-interface Interfejs {
-    private void metodaPrywatna(String a) {
-        System.out.println(a);
-    }
-    static int metodaStatyczna() {
-        return 1;
-    }
 
-    default void metodaDomyslna() {
-        System.out.println("metoda domyslna");
-        metodaPrywatna("Elo");
-    }
-    int zwroc();
-    int poleprywatne = 10;
-}
-class Klasaimplementujaca implements Interfejs{
-    @Override
-    public int zwroc() {
-        return 10;
-    }
-
-}
