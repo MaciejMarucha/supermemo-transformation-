c933d709bac101e486e3386282b2f6fdd11734e7
macmar22 <maciej.marucha@gmail.com>
2021-02-25 22:50:35 +0100

    A

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Interfejs interfejs = new Klasaimplementujaca();
+        System.out.println(Klasaimplementujaca.class);
+        Object obj = interfejs;
+        Class<?> cl = obj.getClass();
+
+
+    }
+}
+interface Interfejs {
+    private void metodaPrywatna(String a) {
+        System.out.println(a);
+    }
+    static int metodaStatyczna() {
+        return 1;
+    }
+
+    default void metodaDomyslna() {
+        System.out.println("metoda domyslna");
+        metodaPrywatna("Elo");
+    }
+    int zwroc();
+    int poleprywatne = 10;
+}
+class Klasaimplementujaca implements Interfejs{
+    @Override
+    public int zwroc() {
+        return 10;
+    }
+
+}
