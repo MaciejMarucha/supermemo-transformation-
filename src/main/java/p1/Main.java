8690678da902e68be0185edd59e93db69c7ed808
Maciek <maciej.marucha@gmail.com>
2023-05-04 23:41:24 +0200

    Initial commit

---
+++ src/main/java/p1/Main.java
@@ -0,0 +1,50 @@
+package p1;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+//zrobic przypadek kiedy sa 2,3 zdania do jednego
+    public static void main(String args[]) throws FileNotFoundException {
+
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get("staryPlik.txt"));
+            PrintWriter writer = new PrintWriter(Paths.get("nowyPlik.txt").toFile(), StandardCharsets.UTF_8);
+            for (int i = 0, lineNumber = 1; i < allLines.size(); i++) {
+                if (!isNumeric(allLines.get(i))) {
+                    if (!allLines.get(i).equals("")) {
+                        writer.println(allLines.get(i));
+                        lineNumber++;
+                    } else {
+                        System.out.println("To jest pusty wiersz");
+                    }
+                    if (lineNumber > 0 && (lineNumber % 3 == 0)) {
+                        writer.println();
+                        lineNumber++;
+                    }
+                }
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
